bb805c82b6b4ae67e7182364dc055cf998e9de9b
Lihuén <110670239+lud-lihuen@users.noreply.github.com>
2022-08-12 08:59:10 -0300

    typos

--- js/validacion.js
+++ js/validacion.js
@@ -1,17 +1,17 @@
 document.getElementById("regBtn").addEventListener("click",function(){
-    let nombre=document.getElementById("nombre")
-    let apellido=document.getElementById("apellido")
-    let email=document.getElementById("email")
-    let password1=document.getElementById("password1")
-    let password2=document.getElementById("password2")
-    let terminos=document.getElementById("terminos")
-    let boton=document.getElementById("regBtn")
+    let nombre=document.getElementById("nombre");
+    let apellido=document.getElementById("apellido");
+    let email=document.getElementById("email");
+    let password1=document.getElementById("password1");
+    let password2=document.getElementById("password2");
+    let terminos=document.getElementById("terminos");
+    let boton=document.getElementById("regBtn");
 
     if(nombre.value!=null && apellido.value!=null && email.value!=null && terminos.checked==true && 
-        (password1.value===password2.value) && password1.value.length>=6){
-        showAlertSuccess()
+        password1.value.length>=6 && (password1.value===password2.value)){
+        showAlertSuccess();
     }else{
-        showAlertError()
+        showAlertError();
     }
 })
 
@@ -23,4 +23,4 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-/* Andrés Martínez, Guido Nadotti, María Pía Camesella, Agustin Gonzatto, Florencia Fernandez, Lihuén Villarreal */
+/* Andrés Martínez, Guido Nadotti, María Pía Camesella, Agustín Gonzatto, Florencia Fernández, Lihuén Villarreal */
