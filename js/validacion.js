6185b3ff440acab63991835af9e76664497cf05c
GuidoNadotti <gpmnadotti@gmail.com>
2022-08-12 08:37:58 -0300

    cambio inicial

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,19 @@
+document.getElementById("regBtn").addEventListener("click",function(){
+    let nombre=document.getElementById("nombre")
+    let apellido=document.getElementById("apellido")
+    let email=document.getElementById("email")
+    let password1=document.getElementById("password1")
+    let password2=document.getElementById("password2")
+    let terminos=document.getElementById("terminos")
+    let boton=document.getElementById("regBtn")
+    for(elemento of document.getElementsByClassName("form-control")){
+        elemento.setAttribute("required","")
+    }
+    if(nombre.value!=null){
+        showAlertSuccess()
+    }
+})
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
